95cb0b0de72f35a7e5adc2e8a5a960c199e82fca
Annika Jusino <annika.jusino@gmail.com>
2018-08-22 00:44:10 -0500

    Added a few more gradle updates, also can now create users on Firebase. Confirmed using second email.

--- app/src/main/java/com/test/annika/timeapp/LoginActivity.java
+++ app/src/main/java/com/test/annika/timeapp/LoginActivity.java
@@ -1,21 +1,36 @@
 package com.test.annika.timeapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+//    private static final String TAG = "LoginActivity";
+
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+//        mAuth = FirebaseAuth.getInstance();
 
         EditText Username = findViewById(R.id.etUsername);
         EditText Password = findViewById(R.id.etPassword);
@@ -23,6 +38,7 @@ public class LoginActivity extends AppCompatActivity {
         Button Login = findViewById(R.id.bLogin);
 
         TextView Register = findViewById(R.id.tvRegister);
+        TextView RUin = findViewById(R.id.tvRUin);
 
         Register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,5 +49,4 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
-
 }
--- app/src/main/java/com/test/annika/timeapp/RegisterActivity.java
+++ app/src/main/java/com/test/annika/timeapp/RegisterActivity.java
@@ -1,30 +1,47 @@
 package com.test.annika.timeapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = "RegisterActivity";
+
+    private FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-         EditText FirstName = findViewById(R.id.etFirstname);
-         EditText LastName = findViewById(R.id.etLastname);
-         EditText Email = findViewById(R.id.etEmail);
-         EditText Phone = findViewById(R.id.etPhone);
-         EditText Username = findViewById(R.id.etUsername);
-         EditText Password = findViewById(R.id.etPassword1);
-         EditText ConfirmPassword = findViewById(R.id.etPassword2);
+        mAuth = FirebaseAuth.getInstance();
 
-         Button Register = findViewById(R.id.bRegister);
+        EditText FirstName = findViewById(R.id.etFirstname);
+        EditText LastName = findViewById(R.id.etLastname);
+        EditText Email = findViewById(R.id.etEmail);
+        EditText Phone = findViewById(R.id.etPhone);
+        EditText Username = findViewById(R.id.etUsername);
+        EditText Password = findViewById(R.id.etPassword1);
+        EditText ConfirmPassword = findViewById(R.id.etPassword2);
+
+        Button Register = findViewById(R.id.bRegister);
 
         //Will need to add something to make sure passwords are the same and if a user already exists
         //Will also probably need to add some regex
@@ -32,12 +49,46 @@ public class RegisterActivity extends AppCompatActivity {
         Register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent BackToLogin = new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(BackToLogin);
-
-                Toast.makeText(RegisterActivity.this, "User created! Please log in.", Toast.LENGTH_SHORT).show();
+                createAccount();
             }
         });
 
     }
+
+    private void createAccount(){
+        EditText Email = findViewById(R.id.etEmail);
+        EditText Password = findViewById(R.id.etPassword1);
+        mAuth.createUserWithEmailAndPassword(Email.getText().toString().trim(),Password.getText().toString().trim())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            Intent BackToLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+                            startActivity(BackToLogin);
+
+                            Toast.makeText(RegisterActivity.this, "User created! Please log in.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        // ...
+                    }
+                });
+    }
+
+
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
 }
